7316aa45bece1578c822cf6fefc871d50ddc8c69
João Paulo Carvalho <9712239j@gmail.com>
2023-12-14 07:45:22 -0300

    adding non null to cliente attributes

--- src/main/java/br/com/farmaciapjr2/springbootapi/model/Cliente.java
+++ src/main/java/br/com/farmaciapjr2/springbootapi/model/Cliente.java
@@ -11,19 +11,31 @@ import lombok.*;
 @Data
 public class Cliente {
     @Id
+    @NonNull
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NonNull
     @JsonProperty("nome")
     private String nome;
+
+    @NonNull
     @JsonProperty("endereco")
     private String endereco;
+
+    @NonNull
     @JsonProperty("telefone")
     private String telefone;
+
+    @NonNull
     @JsonProperty("codigoPostal")
     private String codigoPostal;
+
+    @NonNull
     @JsonProperty("localidade")
     private String localidade;
+
+    @NonNull
     @JsonProperty("numeroContribuinte")
     private String numeroContribuinte;
 }
