ce64b353b93bc8c7b911aa683adbd019c3bb58e8
João Paulo Carvalho <9712239j@gmail.com>
2023-12-14 08:19:31 -0300

    adding nonNull and Builder to TipoProduto

--- src/main/java/br/com/farmaciapjr2/springbootapi/model/TipoProduto.java
+++ src/main/java/br/com/farmaciapjr2/springbootapi/model/TipoProduto.java
@@ -4,14 +4,18 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 
+@Builder
 @Entity
 @Table(name = "tipos_produtos")
 @Data
 public class TipoProduto {
+
+    @NonNull
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NonNull
     @JsonProperty("tipo")
     private String tipo;
 }
