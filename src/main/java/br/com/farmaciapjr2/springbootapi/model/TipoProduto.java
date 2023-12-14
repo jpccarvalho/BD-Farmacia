0b5899b6916a522f1d7f6cce45f7edc3477e1577
João Paulo Carvalho <9712239j@gmail.com>
2023-12-14 09:14:49 -0300

    adding constructor to TipoProduto

--- src/main/java/br/com/farmaciapjr2/springbootapi/model/TipoProduto.java
+++ src/main/java/br/com/farmaciapjr2/springbootapi/model/TipoProduto.java
@@ -5,10 +5,16 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "tipos_produtos")
 @Data
 public class TipoProduto {
+
+    public TipoProduto(@NonNull String tipo) {
+        this.tipo = tipo;
+    }
 
     @NonNull
     @Id
